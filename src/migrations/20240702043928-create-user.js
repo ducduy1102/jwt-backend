a3a0f836665d00e0b6d743ad97f5aad4c239e27f
ducduy1102 <ducduy1110u@gmail.com>
2024-07-03 01:22:58 +0700

    Relationships, add FK for model with ORM

--- src/migrations/20240702043928-create-user.js
+++ src/migrations/20240702043928-create-user.js
@@ -23,6 +23,9 @@ module.exports = {
       phone: {
         type: Sequelize.STRING,
       },
+      username: {
+        type: Sequelize.STRING,
+      },
       groupId: {
         type: Sequelize.INTEGER,
       },
